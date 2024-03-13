741f375586558bdb0e9dd82f28a4ab6862f5f5ea
igmandifari <difariivman@gmail.com>
2024-03-14 02:44:49 +0700

    iniital

---
+++ src/main/java/com/igman/technicaltest/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.igman.technicaltest.repository;
+
+
+import com.igman.technicaltest.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+
+}
